5be8785f60ea735c4e70bbb1e747116ff2d3706d
Nahuel Benitez <106708520+nahuelbenitezs@users.noreply.github.com>
2025-11-25 22:06:29 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Navbar from './section/Navbar'
 import Hero from './section/Hero'
+import About from './section/About'
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
+      <About />
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
-      {/*about*/}
+      
       {/*projects*/}
-      {/*eperience*/}
-      {/*testimonial*/}
       {/*contact*/}
       {/*footer*/}
     </div>
