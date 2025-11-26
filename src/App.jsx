de4b93ca3e96151ae5a740bd39ad61da701429de
Nahuel Benitez <106708520+nahuelbenitezs@users.noreply.github.com>
2025-11-25 21:06:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from './section/Navbar'
+import Hero from './section/Hero'
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+      {/*about*/}
+      {/*projects*/}
+      {/*eperience*/}
+      {/*testimonial*/}
+      {/*contact*/}
+      {/*footer*/}
+    </div>
+  );
+};
+
+export default App;
